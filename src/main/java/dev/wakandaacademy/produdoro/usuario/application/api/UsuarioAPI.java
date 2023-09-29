596f278a23bec452cec393027807bf053ea229b6
Emilli Manzoli <emillimanzoli@gmail.com>
2023-09-29 14:55:36 -0300

    Adicionado swagger

---
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
@@ -0,0 +1,14 @@
+package dev.wakandaacademy.produdoro.usuario.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/v1/usuario")
+public interface UsuarioAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    UsuarioCriadoResponse postNovoUsuario(@RequestBody @Valid UsuarioNovoRequest usuarioNovo);
+}
